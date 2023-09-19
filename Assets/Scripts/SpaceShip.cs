0ede8e8ff54f0f6b5b2b507465daf91cd77a22d5
Silas Peters <silaspeters03@gmail.com>
2023-09-20 00:30:24 +0200

    [SUBMODULE] Added Unity-Essentials

--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -2,12 +2,43 @@ using UnityEngine;
 
 public class SpaceShip : MonoBehaviour
 {
+    public GameObject homePlanet;
+    public float launchForce;
+
+    private Rigidbody _rigidbody;
+    private bool _launched;
+
     void Start()
     {
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        if (!_launched)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Debug.Log("Left");
+                homePlanet.transform.Rotate(0, 0, 1);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Debug.Log("Right");
+                homePlanet.transform.Rotate(0, 0, -1);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space");
+            Launch();
+            _launched = true;
+        }
+    }
+
+    private void Launch()
+    {
+        _rigidbody.AddForce(transform.up * launchForce, ForceMode.Acceleration);
     }
 }
